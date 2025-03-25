705432d2641229435bc8b7ec3c86f5b04d8ad830
Jac0BHenderson <hendejacob@gmail.com>
2025-03-25 16:01:48 +1000

    added logging to LPlate.py

--- LPlate.py
+++ LPlate.py
@@ -4,25 +4,30 @@ from gpiozero import Button
 import signal
 #activate virtual env with source ~/.virtualenvs/pimoroni/bin/activate
 
-inky_display = auto()
+print("Running L Plate Display")
 
-BUTTONS = [5, 6, 16]
-LABELS = ["/home/raspberry/LearnerPlateEInk/L.jpg", "/home/raspberry/LearnerPlateEInk/RedP.jpg", "/home/raspberry/LearnerPlateEInk/GreenP.jpg"]
+try:
+    inky_display = auto()
 
-def display_image(filepath):
-    img = Image.open(filepath)
-    img = img.rotate(-90, expand=True)
-    inky_display.set_image(img)
-    inky_display.show()
+    BUTTONS = [5, 6, 16]
+    LABELS = ["/home/raspberry/LearnerPlateEInk/L.jpg", "/home/raspberry/LearnerPlateEInk/RedP.jpg", "/home/raspberry/LearnerPlateEInk/GreenP.jpg"]
 
-def handle_button(pin):
-    fileName = LABELS[BUTTONS.index(pin.pin.number)]
-    print(f"button press detected on pin {pin}")
-    display_image(fileName)
+    def display_image(filepath):
+        img = Image.open(filepath)
+        img = img.rotate(-90, expand=True)
+        inky_display.set_image(img)
+        inky_display.show()
 
-for pin in BUTTONS:
-    button = Button(pin=pin, pull_up=True, bounce_time=0.1)
-    button.when_pressed = handle_button
-    
+    def handle_button(pin):
+        fileName = LABELS[BUTTONS.index(pin.pin.number)]
+        print(f"button press detected on pin {pin}")
+        display_image(fileName)
 
-signal.pause()                   
+    for pin in BUTTONS:
+        button = Button(pin=pin, pull_up=True, bounce_time=0.1)
+        button.when_pressed = handle_button
+        
+
+    signal.pause()              
+except Exception as ex:
+    print(ex)     
