4bff481dd29294a29c8b8ce3b87b8c23ad1013f0
Jac0BHenderson <hendejacob@gmail.com>
2025-03-25 16:17:17 +1000

    added more logging

--- LPlate.py
+++ LPlate.py
@@ -9,25 +9,30 @@ print("Running L Plate Display")
 try:
     inky_display = auto()
 
-    BUTTONS = [5, 6, 16]
-    LABELS = ["/home/pi/AutoLPlate/L.jpg", "/home/pi/AutoLPlate/RedP.jpg", "/home/pi/AutoLPlate/GreenP.jpg"]
+    print("Getting Image files")
+    LPlate = Image.open("/home/pi/AutoLPlate/L.jpg").rotate(-90, expand=True)
+    RedPPlate = Image.open("/home/pi/AutoLPlate/RedP.jpg").rotate(-90, expand=True)
+    GreenPPlate = Image.open("/home/pi/AutoLPlate/GreenP.jpg").rotate(-90, expand=True)
+    print("Images loaded")
 
-    def display_image(filepath):
-        img = Image.open(filepath)
-        img = img.rotate(-90, expand=True)
+    BUTTONS = {
+        5 : LPlate, 
+        6 : RedPPlate, 
+        16 : GreenPPlate
+    }
+
+    def display_image(img):
         inky_display.set_image(img)
         inky_display.show()
 
     def handle_button(pin):
-        fileName = LABELS[BUTTONS.index(pin.pin.number)]
         print(f"button press detected on pin {pin}")
-        display_image(fileName)
+        display_image(BUTTONS[pin])
 
-    for pin in BUTTONS:
+    for pin in BUTTONS.keys():
         button = Button(pin=pin, pull_up=True, bounce_time=0.1)
         button.when_pressed = handle_button
         
-
     signal.pause()              
 except Exception as ex:
     print(ex)     
